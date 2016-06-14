bda59c59b377c1f445ca9fae24be4a6ba9d72345
Gabriel Mojica <niko.mojica@live.com>
2016-06-15 01:58:38 +1000

    fix: Remove fab button on MainActivity

--- app/src/main/java/com/example/gabriel/habitsquare/MainActivity.java
+++ app/src/main/java/com/example/gabriel/habitsquare/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.gabriel.habitsquare;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -19,15 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null);
-            }
-        });
 
         Habits habits = new Habits();
         Habit brushTeeth = new Habit("Brush Teeth");

8ae6b8e31abf1329b8a7b139495bca32ac4f50c7
Gabriel Mojica <niko.mojica@live.com>
2016-06-15 03:09:40 +1000

    feat: Create a createHabitActivity activity

---
+++ app/src/main/java/com/example/gabriel/habitsquare/CreateHabitActivity.java
@@ -0,0 +1,15 @@
+package com.example.gabriel.habitsquare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateHabitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_habit);
+
+
+    }
+}
--- app/src/main/java/com/example/gabriel/habitsquare/MainActivity.java
+++ app/src/main/java/com/example/gabriel/habitsquare/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gabriel.habitsquare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -38,16 +39,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.new_habit:
+                startCreateHabitActivity();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startCreateHabitActivity() {
+        Intent intent = new Intent(this, CreateHabitActivity.class);
+        startActivity(intent);
     }
 }

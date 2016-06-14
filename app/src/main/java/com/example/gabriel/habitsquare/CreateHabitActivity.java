17644b051e89b20b693b82eb558f994deeb854e1
Gabriel Mojica <niko.mojica@live.com>
2016-06-15 03:42:19 +1000

    feat: Add error validation when creating habit

--- app/src/main/java/com/example/gabriel/habitsquare/CreateHabitActivity.java
+++ app/src/main/java/com/example/gabriel/habitsquare/CreateHabitActivity.java
@@ -1,15 +1,35 @@
 package com.example.gabriel.habitsquare;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
 
-public class CreateHabitActivity extends AppCompatActivity {
+public class CreateHabitActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText mHabitTitleEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_habit);
 
+        mHabitTitleEditText = (EditText) findViewById(R.id.habit_title_edittext);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.create_habit_fab);
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String habittitle = mHabitTitleEditText.getText().toString().trim();
+        if (TextUtils.isEmpty(habittitle)) {
+            mHabitTitleEditText.setError("this ");
+        } else {
+            Snackbar.make(v, "This works", Snackbar.LENGTH_LONG).show();
+        }
     }
 }
